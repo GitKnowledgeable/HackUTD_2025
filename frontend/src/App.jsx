a7336e6dc311be257ff3beba488ad4f1231663e8
Suvan <suvan@atxscioly.org>
2025-11-08 19:50:33 -0600

    debug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Browse from './pages/Browse'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/browse" element={<Browse />} />
         </Routes>
       </Router>
     </>
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,13 @@
 import '../css/SearchBar.css'
 export default function SearchBar() {
     return (
-        <div className="search-bar">
-            Hello
-        </div>
+        <>
+            <div className="search-bar">
+                Hello
+            </div>
+            <a href="/browse">
+                <button>Go to Browse Page</button>
+            </a>
+        </>
     )
 }
